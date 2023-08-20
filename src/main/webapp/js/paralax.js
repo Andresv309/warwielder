00057c531d67dab62f5d8420d41a73e0ee9ea794
Cristian Vega <crisda.arive.1503@gmail.com>
2023-08-20 05:05:57 -0500

    Fix paralax file

--- src/main/webapp/js/paralax.js
+++ src/main/webapp/js/paralax.js
@@ -1,1 +1,26 @@
-console.log("holaaaas");
+const element = document.getElementById('skin');
+const height = element.clientHeight;
+const width = element.clientWidth;
+
+element.style.transformStyle = 'preserve-3d';
+
+document.addEventListener('mousemove', (event) => {
+	const { layerX, layerY } = event;
+
+	const yRotation = ((layerX - width / 2) / width) * 20;
+	const xRotation = ((layerY - height / 2) / height) * 20;
+
+	const string = `
+		perspective(400px)
+		scale(1.1)
+		rotateX(${xRotation}deg)
+		rotateY(${yRotation}deg)
+	`;
+
+//	element.style.transform = string;
+	element.style.webkitTransform = string; 
+//	element.style.MozTransform = string; 
+//	element.style.msTransform = string; 
+
+	console.log({ yRotation, xRotation });
+});
