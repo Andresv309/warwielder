49a07cfd547babdea24224611b98d51d10a935f6
Andresv309 <andresv309@gmail.com>
2023-08-22 16:05:06 -0500

    Create servlet for updating user

---
+++ src/main/java/com/adso/apiServlets/UserInfoServlet.java
@@ -0,0 +1,88 @@
+package com.adso.apiServlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.adso.dao.DAOManagerImp;
+import com.adso.dao.interfaces.UserDAO;
+import com.adso.entities.Pet;
+import com.adso.entities.User;
+import com.adso.exceptions.auth.NotFoundAuthToken;
+import com.adso.exceptions.auth.NotValidAuthToken;
+import com.adso.exceptions.pets.NotFoundPetException;
+import com.adso.exceptions.user.NotValidUserParamsException;
+import com.adso.exceptions.user.UserNotFoundException;
+import com.adso.utils.JsonResponseBuilder;
+import com.adso.utils.Utils;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "userInfoServlet", urlPatterns = "/api/v1/users") 
+public class UserInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 4981069403773795626L;
+	private UserDAO userDao;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		this.userDao = new DAOManagerImp().getUserDAO();
+		super.init(config);
+	}
+	@Override
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		JsonResponseBuilder jsonBuilder  = JsonResponseBuilder.create();
+
+		try {
+			Long userId = Utils.getUserIdFromCookies(request);
+	        String jsonBody = Utils.stringifyJsonBody(request);
+	        
+	        // Parse the JSON data using Gson
+	        JsonObject jsonObject = JsonParser.parseString(jsonBody).getAsJsonObject();
+	        
+	        if (!jsonObject.has("petId")) {
+	        	throw new NotValidUserParamsException();
+	        }
+
+        	Long petId = jsonObject.get("petId").getAsLong();
+        	Pet pet = new Pet();
+        	pet.setId(petId);
+        	
+        	User userUpdated = userDao.updateUserInfo(pet, userId);
+        	User userToSend = new User();
+        	userToSend.setPet(userUpdated.getPet());
+        	userToSend.setCoins(userUpdated.getCoins());
+        	userToSend.setId(userUpdated.getId());
+        	userToSend.setUsername(userUpdated.getUsername());
+
+			jsonBuilder.addField("data", userToSend);
+
+        } catch (
+			UserNotFoundException |
+			NotFoundAuthToken |
+			NotValidAuthToken |
+			NotValidUserParamsException |
+			NotFoundPetException
+			e
+		) {
+			jsonBuilder.addField("error", e.getMessage());
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } catch (NumberFormatException e) {
+        	jsonBuilder.addField("error", "Invalid pet ID format.");
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(jsonBuilder.build());
+	}
+
+}
