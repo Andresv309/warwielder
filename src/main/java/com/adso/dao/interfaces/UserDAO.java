d9b8095f1b69dec8ccab4c76b38888f952353b8f
Andresv309 <andresv309@gmail.com>
2023-08-21 20:45:49 -0500

    Refactor interfaces to return proper values

--- src/main/java/com/adso/dao/interfaces/UserDAO.java
+++ src/main/java/com/adso/dao/interfaces/UserDAO.java
@@ -1,13 +1,23 @@
 package com.adso.dao.interfaces;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.adso.entities.Card;
+import com.adso.entities.DeckCard;
 import com.adso.entities.User;
+import com.adso.exceptions.decks.CardAlreadyInDeckException;
+import com.adso.exceptions.decks.NotValidPositionValue;
+import com.adso.exceptions.user.UserAlreadyExistsException;
+import com.adso.exceptions.user.UserUnauthorizedForOperationException;
 
 public interface UserDAO extends DAO<User, Long> {
 //	String getUserInfo(Long id);
 //	String getUserDecks(Long id);
-	String getUserUnlockedCards(Long id);
-	String updateDeck(String updateDeckCardJson, Long userId);
-	User addNewUser(String newUserJson);
-	String getUserDecksCards(Long id);
+	Set<Card> getUserUnlockedCards(Long id);
+	DeckCard updateDeck(DeckCard updateDeckCard, Long userId) throws NotValidPositionValue, UserUnauthorizedForOperationException, CardAlreadyInDeckException;
+	User addNewUser(String username, String password) throws UserAlreadyExistsException;
+	List<Object> getUserDecksCards(Long userId);
 	String redeemCardCode(String redeemCodeJson, Long userId);
 }
