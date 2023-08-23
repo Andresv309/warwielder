abaf59df082975351aefb96b2640eec4a21a9169
Andresv309 <andresv309@gmail.com>
2023-08-22 23:39:32 -0500

    Add exceptions

---
+++ src/main/java/com/adso/exceptions/app/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.adso.exceptions.app;
+
+public class NotFoundException extends Exception {
+	private static final long serialVersionUID = 1953544789285698359L;
+
+	public NotFoundException (String cause) {
+//		super("The pet with the id: (" + petId + ") was not found.");
+
+	}
+	
+}
