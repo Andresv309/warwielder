671193dc059bfd0dacbe5efb614151963674abca
Cristian Vega <crisda.arive.1503@gmail.com>
2023-08-23 23:33:37 -0500

    Create servlet Register

---
+++ src/main/java/com/adso/servlets/Register.java
@@ -0,0 +1,21 @@
+package com.adso.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "register", urlPatterns = "/register")
+public class Register extends HttpServlet {     
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Estoy en el RegisterPage\n");
+
+		getServletContext().getRequestDispatcher("/pages/register.jsp").forward(request, response);
+	}
+
+}
