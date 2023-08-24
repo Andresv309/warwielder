1a91ec914491fb20619b0606cd789df0268beeae
Cristian Vega <crisda.arive.1503@gmail.com>
2023-08-24 17:02:58 -0500

    Fix in register.js

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -1,25 +1,29 @@
-    const nextStepButton = document.getElementById('nextStep');
-    const submitButton = document.getElementById('submitForm');
-    const step1Section = document.getElementById('step1');
-    const step2Section = document.getElementById('step2');
-    const submitButtonBefore = submitButton.querySelector("::before");
+const nextStepButton = document.getElementById('nextStep');
+const submitButton = document.getElementById('submitForm');
+const step1Section = document.getElementById('step1');
+const step2Section = document.getElementById('step2');
 
-    nextStepButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        // Aquí podrías realizar validaciones y procesar los datos del primer paso
+nextStepButton.addEventListener('click', function(event) {
+	event.preventDefault();
+	// Aquí podrías realizar validaciones y procesar los datos del primer paso
 
-        // Oculta el primer paso y muestra el segundo paso
-        step1Section.style.display = 'none';
-        step2Section.style.display = 'block';
-    });
+	// Oculta el primer paso y muestra el segundo paso
+	step1Section.style.display = 'none';
+	step2Section.style.display = 'block';
+});
 
-    submitButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        console.log(submitButtonBefore)
-        
-        // Aquí podrías realizar validaciones y enviar los datos del segundo paso
-		submitButtonBefore.style.content = 'Empezar'; 
-        // Simplemente para este ejemplo, podrías redirigir al usuario a una página de éxito
-        window.location.href = 'registro_exitoso.jsp';
-    });
+submitButton.addEventListener('click', function(event) {
+	event.preventDefault();
+
+	// Aquí podrías realizar validaciones y enviar los datos del segundo paso
+	// Simplemente para este ejemplo, podrías redirigir al usuario a una página de éxito
+	window.location.href = 'registro_exitoso.jsp';
+});
+
+
+let swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+ }); 
