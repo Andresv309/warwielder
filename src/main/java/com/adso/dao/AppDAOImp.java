c9e9e76fd1d3914e87a6d4a62e3fbc37b15568bd
Andresv309 <andresv309@gmail.com>
2023-08-25 17:44:42 -0500

    Refactor get cards method to do pagination

--- src/main/java/com/adso/dao/AppDAOImp.java
+++ src/main/java/com/adso/dao/AppDAOImp.java
@@ -1,14 +1,23 @@
 package com.adso.dao;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.adso.dao.interfaces.AppDAO;
 import com.adso.entities.Card;
 import com.adso.entities.Pet;
+import com.adso.enums.Race;
+import com.adso.enums.Rarity;
 import com.adso.exceptions.cards.NotFoundCardException;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 
 public class AppDAOImp implements AppDAO {
 	
@@ -17,11 +26,57 @@ public class AppDAOImp implements AppDAO {
     public AppDAOImp(EntityManagerFactory emf) {
         this.emf = emf;
     }
+    
+    
+    public Long getTotalNumberOfCards() {
+    	EntityManager em = emf.createEntityManager();
+    	
+    	TypedQuery<Long> countQuery  = em.createQuery("SELECT COUNT(c) FROM Card c", Long.class);
+    	Long totalCount = countQuery.getSingleResult();
+  
+    	em.close();
+    	
+    	return totalCount;
+    }
+    
+    
+    
+    private Predicate[] buildPredicates(Map<String, Object> queryParams, CriteriaBuilder cb, Root<Card> root) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        // Add your filtering conditions based on query parameters here
+        Rarity rarity = (Rarity) queryParams.get("rarity");
+        if (rarity != null) {
+            predicates.add(cb.equal(root.get("rarity"), rarity));
+        }
+        
+        Race race = (Race) queryParams.get("race");
+        if (race != null) {
+            predicates.add(cb.equal(root.get("race"), race));
+        }
+
+        return predicates.toArray(new Predicate[0]);
+    }
 
 	@Override
-	public List<Card> getAppCards() {
-		EntityManager em = emf.createEntityManager();		
-		List<Card> appCards = em.createQuery("FROM Card", Card.class).getResultList();
+	public List<Card> getAppCards(Map<String, Object> queryParams) {
+		EntityManager em = emf.createEntityManager();	
+		
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+	    CriteriaQuery<Card> query = cb.createQuery(Card.class);
+	    Root<Card> root = query.from(Card.class);
+	    
+	    Predicate[] predicates = buildPredicates(queryParams, cb, root);
+
+	    query.select(root)
+	         .where(predicates);
+
+	    TypedQuery<Card> typedQuery = em.createQuery(query);
+        typedQuery.setFirstResult((int) queryParams.get("offset"));
+        typedQuery.setMaxResults((int) queryParams.get("limit"));
+
+	    List<Card> appCards = typedQuery.getResultList();
+
 		em.close();
 
 		return appCards;
--- src/main/java/com/adso/dao/interfaces/AppDAO.java
+++ src/main/java/com/adso/dao/interfaces/AppDAO.java
@@ -1,13 +1,15 @@
 package com.adso.dao.interfaces;
 
 import java.util.List;
+import java.util.Map;
 
 import com.adso.entities.Card;
 import com.adso.entities.Pet;
 import com.adso.exceptions.cards.NotFoundCardException;
 
 public interface AppDAO {
-	List<Card> getAppCards();
+	List<Card> getAppCards(Map<String, Object> queryParams);
 	Card getAppCard(Long id) throws NotFoundCardException;
 	List<Pet> getAppPets();
+	Long getTotalNumberOfCards();
 }
