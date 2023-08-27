77b0be82c84acfacac6db49412ba4b234dfa495e
Andresv309 <andresv309@gmail.com>
2023-08-27 02:28:05 -0500

    Make method accesible

--- src/main/java/com/adso/utils/CuidGenerator.java
+++ src/main/java/com/adso/utils/CuidGenerator.java
@@ -4,7 +4,7 @@ import io.github.thibaultmeyer.cuid.CUID;
 
 public class CuidGenerator {
 	
-	static String generate () {
+	public static String generate () {
 		final int customLength = 12;  
 		final CUID cuid = CUID.randomCUID2(customLength);
 		return cuid.toString();
