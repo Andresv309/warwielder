07cecf8bcdb96949a5dbe90c31b349a7e8d2b307
Cristian Vega <crisda.arive.1503@gmail.com>
2023-09-04 23:42:59 -0500

    Fix CardsPage js

--- src/main/webapp/js/cardsPage.js
+++ src/main/webapp/js/cardsPage.js
@@ -1,11 +1,14 @@
-const imagesPath = "http://localhost:8080/warwielder/api/v1/cardImages/" 
-let imagesArray = []
-const cardsContainer = document.getElementById("cardsContainer")
+const imagesEndpoint = "/api/v1/assets/cards" 
+const basePath = "http://localhost:8080/warwielder"
+const imagePath = basePath + imagesEndpoint
 
-for(let i = 1; i <= 32; i++ ){
-  const imageRoute = imagesPath + `card_${i}.png`
-  imagesArray.push(imageRoute)
-}
+//let imagesArray = []
+const cardsContainer = document.getElementById("cardsContainer")
+//
+//for(let i = 1; i <= 32; i++ ){
+//  const imageRoute = imagesPath + `card_${i}.png`
+//  imagesArray.push(imageRoute)
+//}
 
 
 function htmlRenderer (htmlElement, htmlChildrenArray, htmlComponent) {
@@ -14,10 +17,39 @@ function htmlRenderer (htmlElement, htmlChildrenArray, htmlComponent) {
 
 
 function cardTemplateComponent(imgPath) {
-  return `<img width="240px" alt="bang" src=${imgPath}>`
+	
+  return `<img class="card" width="240px" alt="bang" src=${imgPath}>`
 }
 
 
 // cardsContainer.innerHTML = imagesArray.reduce((accumulator, currentValue) => accumulator + cardTemplateComponent(currentValue), "")
 
-htmlRenderer(cardsContainer, imagesArray, cardTemplateComponent)
+
+
+function renderCards(cards) {
+	const cardsImages =  cards.map(card => imagePath + "/" + card.img)
+	htmlRenderer(cardsContainer, cardsImages, cardTemplateComponent)
+}
+
+
+
+async function fetchCards(){
+	const cardsPath = basePath + "/api/v1/cards?offset=0&limit=20"
+
+	
+//	const res = await fetch(cardsPath)
+//	const cardsData = await res.json()
+	const cardsData = await fetchWrapper(cardsPath)
+
+	console.log(cardsData) 
+	return cardsData.data	
+} 
+
+
+async function main() {
+	const cards =  await fetchCards()
+    renderCards(cards)
+} 
+
+
+main();
