088c5d931d2dda2d026bd7b2e02b4a2d4f76cf79
Cristian Vega <crisda.arive.1503@gmail.com>
2023-09-04 23:43:09 -0500

    Fix paralax

--- src/main/webapp/js/paralax.js
+++ src/main/webapp/js/paralax.js
@@ -1,23 +1,40 @@
-const element = document.getElementById('skin');
-const height = element.clientHeight;
-const width = element.clientWidth;
+function addInteractionToCards () {
+	setTimeout(() => {
+		const elements = document.querySelectorAll('.card');
+		
+		console.log(elements)
+		
+		
+		elements.forEach(element => {
+				
+						
+			const height = element.clientHeight;
+			const width = element.clientWidth;
+			
+			element.style.transformStyle = 'preserve-3d';
+			
+			element.addEventListener('mousemove', (event) => {
+			    const { layerX, layerY } = event;
+			
+			    const yRotation = ((layerX - width / 2) / width) * 20;
+			    const xRotation = ((layerY - height / 2) / height) * 20;
+			
+			    const string = `
+			        perspective(400px)
+			        scale(1.1)
+			        rotateX(${xRotation}deg)
+			        rotateY(${yRotation}deg)
+			    `;
+			
+			    element.style.transform = string; 
+			});
+			
+			
+		})
+		
 
-element.style.transformStyle = 'preserve-3d';
+	}, 1000)
+}
 
-element.addEventListener('mousemove', (event) => {
-	const { layerX, layerY } = event;
 
-	const yRotation = ((layerX - width / 2) / width) * 20;
-	const xRotation = ((layerY - height / 2) / height) * 20;
-
-	const string = `
-		perspective(400px)
-		scale(1.1)
-		rotateX(${xRotation}deg)
-		rotateY(${yRotation}deg)
-	`;
-
-	element.style.webkitTransform = string; 
- 
-//	console.log({ yRotation, xRotation });
-});
+addInteractionToCards()
