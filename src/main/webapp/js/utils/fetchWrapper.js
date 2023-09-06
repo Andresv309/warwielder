21a29f44a5e4daeec67f7969953efcca1e9f2c83
Cristian Vega <crisda.arive.1503@gmail.com>
2023-09-05 23:27:22 -0500

    Add FetchWrapper.js

--- src/main/webapp/js/utils/fetchWrapper.js
+++ src/main/webapp/js/utils/fetchWrapper.js
@@ -1,10 +1,23 @@
+
+
+/**
+ * Wraps the Fetch API to simplify making HTTP requests and handling responses.
+ *
+ * @param {string} url - The URL to send the HTTP request to.
+ * @param {string} method - The HTTP request method (default is 'GET').
+ * @param {object} body - The data to send in the request body (optional).
+ *
+ * @returns {Promise} A Promise that resolves to the parsed JSON response or null on error.
+ */
+
 async function fetchWrapper(url, method = 'GET', body) {
-	try{
-		
+	try {
+		// Define request headers with a default content type of JSON.
 		const headers  = {
 			"content-Type": "application/json"
 		} 
 		
+		// Configure the request options.
 		const options =  {
 			method,
 			headers,
@@ -12,25 +25,24 @@ async function fetchWrapper(url, method = 'GET', body) {
 			
 		}
 		
+		// Send the HTTP request and await the response.
 		const response  = await fetch(url,options)
 		
-		
-//		if(response.ok){
-			try{
-				const data = await response.json()
-				return data
-			} catch(error) {
-				cosole.log("Error parsing JSON:", error)
-				return null
-			}
-//		}else {
-//			cosole.error("Fetch failed with status:", response.status)
-//			return null
-//		}
-		
-	}catch (error){
-		cosole.error("Fetch Error:", error)
+		try{
+			// Try to parse the response body as JSON and return it.
+			const data = await response.json()
+			return data
+		} catch(error) {
+			console.log("Error parsing JSON:", error)
+			return null
+		}
+
+	} catch (error){
+		console.error("Fetch Error:", error)
 		return null
-		
 	}
 } 
+
+
+
+export { fetchWrapper }
