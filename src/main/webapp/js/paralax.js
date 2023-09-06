8c9230be32b06184006b283650720ffc9e5802b1
Cristian Vega <crisda.arive.1503@gmail.com>
2023-09-05 23:26:58 -0500

    Fix Paralax Js

--- src/main/webapp/js/paralax.js
+++ src/main/webapp/js/paralax.js
@@ -1,40 +1,25 @@
-function addInteractionToCards () {
-	setTimeout(() => {
-		const elements = document.querySelectorAll('.card');
-		
-		console.log(elements)
-		
-		
-		elements.forEach(element => {
-				
-						
-			const height = element.clientHeight;
-			const width = element.clientWidth;
-			
-			element.style.transformStyle = 'preserve-3d';
-			
-			element.addEventListener('mousemove', (event) => {
-			    const { layerX, layerY } = event;
-			
-			    const yRotation = ((layerX - width / 2) / width) * 20;
-			    const xRotation = ((layerY - height / 2) / height) * 20;
-			
-			    const string = `
-			        perspective(400px)
-			        scale(1.1)
-			        rotateX(${xRotation}deg)
-			        rotateY(${yRotation}deg)
-			    `;
-			
-			    element.style.transform = string; 
-			});
-			
-			
-		})
-		
 
-	}, 1000)
+function paralaxEffect (htmlElement) {
+	const height = htmlElement.clientHeight;
+	const width = htmlElement.clientWidth;
+	
+	htmlElement.style.transformStyle = 'preserve-3d';
+	
+	htmlElement.addEventListener('mousemove', (event) => {
+	    const { layerX, layerY } = event;
+	
+	    const yRotation = ((layerX - width / 2) / width) * 20;
+	    const xRotation = ((layerY - height / 2) / height) * 20;
+	
+	    const effect = `
+	        perspective(400px)
+	        scale(1.1)
+	        rotateX(${xRotation}deg)
+	        rotateY(${yRotation}deg)
+	    `;
+	
+	    htmlElement.style.transform = effect; 
+	});
 }
 
-
-addInteractionToCards()
+export { paralaxEffect }
