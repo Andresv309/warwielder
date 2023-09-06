dd75950b444d5047bd8922abb0b5b37a56cba234
Cristian Vega <crisda.arive.1503@gmail.com>
2023-09-05 23:26:19 -0500

    Add endPoints.js

---
+++ src/main/webapp/constants/endpoints.js
@@ -0,0 +1,7 @@
+const basePath = "http://localhost:8080/warwielder"
+const cardImagesEndpoint = basePath + "/api/v1/assets/cards"
+const cardsEndpoint = basePath + "/api/v1/cards?offset=0&limit=20"
+
+
+
+export { cardImagesEndpoint, cardsEndpoint }
