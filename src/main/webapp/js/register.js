773cc3387b2f704ba0fbf5041c087789b95d5d49
Cristian Vega <crisda.arive.1503@gmail.com>
2023-09-07 23:26:48 -0500

    Fix Register.js

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -3,27 +3,26 @@ const submitButton = document.getElementById('submitForm');
 const step1Section = document.getElementById('step1');
 const step2Section = document.getElementById('step2');
 
-
 const heart = document.querySelectorAll('.button-heart')
 
-console.log(step2Section)
-step1Section.style.display = 'none';
-step2Section.style.display = 'block';
+step2Section.style.display = 'none';
 
 nextStepButton.addEventListener('click', function(event) {
 	event.preventDefault();
 	// Aquí podrías realizar validaciones y procesar los datos del primer paso
 
 	// Oculta el primer paso y muestra el segundo paso
-//	step1Section.style.display = 'none';
-//	step2Section.style.display = 'block';
-//	step2Section.style.visibility = 'visible';
+	step1Section.style.display = 'none';
+	step2Section.style.display = 'block';
+	step2Section.style.visibility = 'visible';
 });
 
 submitButton.addEventListener('click', function(event) {
 	event.preventDefault();
 
 	// Aquí podrías realizar validaciones y enviar los datos del segundo paso
+	
+	
 	// Simplemente para este ejemplo, podrías redirigir al usuario a una página de éxito
 	window.location.href = 'registro_exitoso.jsp';
 });
@@ -50,6 +49,8 @@ heartButton.addEventListener('click', (e) => {
 	    heartElement.classList.add("is-active");
 	}
 })
+
+
 
 
 
