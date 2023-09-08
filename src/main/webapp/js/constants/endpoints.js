e693134335bc9217057b40a1ee4eb37aa16b3540
Cristian Vega <crisda.arive.1503@gmail.com>
2023-09-07 23:23:44 -0500

    Fix endpoints.js

--- src/main/webapp/js/constants/endpoints.js
+++ src/main/webapp/js/constants/endpoints.js
@@ -1,7 +1,8 @@
 const basePath = "http://localhost:8080/warwielder"
 const cardImagesEndpoint = basePath + "/api/v1/assets/cards"
 const cardsEndpoint = basePath + "/api/v1/cards?offset=0&limit=20"
+const petsEndpoint = basePath + "/api/v1/pets"
 
 
 
-export { cardImagesEndpoint, cardsEndpoint }
+export { cardImagesEndpoint, cardsEndpoint, petsEndpoint}
